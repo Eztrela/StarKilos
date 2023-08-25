377a12ac037f3de85be11daba32357818e88f7ae
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-08-25 13:06:51 -0300

    Adicionando classe inicial de Cliente (#1)

---
+++ classes/Cliente.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+	private int id;
+	private List<Pesagem> listaDePesagem;
+	
+	public Cliente(int id) {
+		this.id = id;
+		this.listaDePesagem = new ArrayList<>();
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Pesagem> getListaDePesagem() {
+		return listaDePesagem;
+	}
+
+	public void setListaDePesagem(List<Pesagem> listaDePesagem) {
+		this.listaDePesagem = listaDePesagem;
+	}
+
+}
