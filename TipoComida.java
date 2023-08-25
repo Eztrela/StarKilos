c7c4d2af4230933f1a205ecca0dae7ae6d217acd
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-08-25 13:15:46 -0300

    criando classe TipoComida (#3)
    * criando classe TipoComida
    
    * implementando classe tipoComida
    
    * alterando nome do arquivo e classe para TipoComida

---
+++ TipoComida.java
@@ -0,0 +1,34 @@
+package model;
+
+public class TipoComida {
+	private String nome;
+	private double preco;
+	
+	public TipoComida(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoPesagem [nome=" + nome + ", preco=" + preco + "]";
+	}
+	
+	
+}
