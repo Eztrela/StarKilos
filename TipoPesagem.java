2fc2fee73ef359226f401eec22eabd894a0af188
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-08-25 13:12:21 -0300

    implementando classe tipoComida

--- TipoPesagem.java
+++ TipoPesagem.java
@@ -1,5 +1,34 @@
 package model;
 
 public class TipoPesagem {
+	private String nome;
+	private double preco;
+	
+	public TipoPesagem(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoPesagem [nome=" + nome + ", preco=" + preco + "]";
+	}
+	
+	
 }
